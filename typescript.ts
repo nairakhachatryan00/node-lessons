da5561cebf0369553547beb1d038ce2e0e44b615
naira <nairakhachatryan00@gmail.com>
2021-07-25 22:38:59 +0400

    typescript

---
+++ typescript.ts
@@ -0,0 +1,112 @@
+abstract class Car {
+    name: string;
+    public getDesc(): string {
+        return `${this.name}`;
+    }
+    public abstract getInfo(): string;
+    public abstract cost():number;
+}
+
+//`BMW with color green decorated and price 10200$ and your car has hybrid engine`.
+
+
+class Toyota extends Car {
+    name = 'Toyota';
+    getInfo(): string {
+        return this.getDesc() + ` and price ${this.cost()}$`;
+    };
+    cost(): number {
+        return 1000;
+    }
+}
+
+abstract class CarOptions extends Car{
+    decoratedCar: Car;
+    abstract getInfo(): string;
+    abstract cost(): number;
+}
+
+class Black extends CarOptions {
+    decoratedCar: Car;
+
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+    }
+
+    getDesc(): string {
+        return this.decoratedCar.getDesc() + ` with black color decorated`;
+    }
+
+    getInfo(): string {
+        return this.getDesc() + ` and price ${this.cost()}$`;
+    }
+
+    cost(): number {
+        return this.decoratedCar.cost() + 100;
+    }
+}
+
+class FourDoors extends CarOptions {
+    decoratedCar: Car;
+
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+    }
+
+    getDesc(): string {
+        return this.decoratedCar.getDesc() + ' and has 4 doors';
+    }
+
+    getInfo(): string {
+        return this.getDesc() + ` and price ${this.cost()}$`;
+    }
+
+    cost(): number {
+        return this.decoratedCar.cost() + 200;
+    }
+}
+
+
+class Hybrid extends CarOptions {
+    decoratedCar: Car;
+
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+    }
+
+    getDesc(): string {
+        return this.decoratedCar.getDesc() + ' and your car has hybrid engine';
+    }
+
+    getInfo(): string {
+        return this.getDesc() + ` and price ${this.cost()}$`;
+    }
+
+    cost(): number {
+        return this.decoratedCar.cost() + 300;
+    }
+}
+
+class OilBased extends CarOptions {
+    decoratedCar: Car;
+
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+    }
+
+    getInfo(): string {
+        return this.decoratedCar.getInfo() + 'oil based engine.';
+    }
+    cost(): number {
+        return this.decoratedCar.cost() + 100;
+    }
+}
+let myCar = new Toyota(); //output "Toyota and price 1000$"
+// myCar = new Black(myCar);  //output "Toyota with black color decorated and price 1000$"
+// myCar = new FourDoors(new Black(myCar)); //output "Toyota with black color decorated and has 4 doors and price 1000$"
+myCar = new Hybrid(new FourDoors(new Black(myCar))); //output "Toyota with black color decorated and has 4 doors and your car has hybrid engine and price 1000$"
+console.log(myCar.getInfo());
